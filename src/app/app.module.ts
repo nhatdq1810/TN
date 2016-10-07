a71b326f7d9913a908e808e7e891daefc2079ca3
Dang Quang Nhat <nhat.dang@sutrixsolutions.com>
2016-10-07 18:08:58 +0700

    use ng2-uploader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { FooterComponent } from './footer/footer.component';
 import { FormValidationComponent } from './form-validation/form-validation.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { Nouislider } from 'ng2-nouislider';
+import { UPLOAD_DIRECTIVES } from 'ng2-uploader';
+
 
 
 @NgModule({
@@ -25,7 +27,8 @@ import { Nouislider } from 'ng2-nouislider';
     FooterComponent,
     FormValidationComponent,
     Nouislider,
-    SearchResultComponent
+    SearchResultComponent,
+    UPLOAD_DIRECTIVES
   ],
   imports: [
     BrowserModule,
--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-search-result',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  private zone: NgZone;
+  private hasBaseDropZoneOver: boolean = false;
+  private progress: number = 0;
+  private response: any = {};
+  private options: Object;
+  private uploadEvents: EventEmitter<any>;
+  private previewData: any;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.zone = new NgZone({enableLongStackTrace: false});
+    this.options = {
+      url: 'https://api.imgur.com/3/upload',
+      calculateSpeed: true,
+      customHeaders: {'Authorization': 'Client-ID f9ea705cdc1e4c9'},
+      previewUrl: true
+    };
+    this.uploadEvents = new EventEmitter();
+  }
+
+  handleUpload(data: any): void {
+    console.log(data);
+    this.zone.run(() => {
+      this.response = data;
+      this.progress = Math.floor(data.progress.percent / 100);
+    });
+  }
+
+  handlePreviewData(data: any): void {
+    this.previewData = data;
+  }
+
+  // fileOverBase(e: any): void{
+  //   this.hasBaseDropZoneOver = e;
+  // }
+
+  startUpload(){
+    this.uploadEvents.emit('startUpload');
   }
 
 }
