d4c3018a9ec669312dbdcc0939f0e1dcf37eb85b
Dang Quang Nhat <nhat.dang@sutrixsolutions.com>
2016-11-02 17:57:57 +0700

    modify css for 404 component

--- src/app/components/404/404.component.ts
+++ src/app/components/404/404.component.ts
@@ -7,9 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
-  constructor() { }
+  private deals: Array<any>;
+
+  constructor() {
+    // this.init();
+    this.fakeInit();
+  }
 
   ngOnInit() {
+  }
+
+  init() {
+
+  }
+
+  fakeInit() {
+    this.deals = [
+      {
+        id: 2,
+        hinhanh: 'assets/img/index-03.jpg',
+        diachi: '123 abc P.15 Quận Gò Vấp',
+        songuoi: 2,
+        dientich: 25,
+        gioitinh: 'nam',
+        truong: 'PTIT',
+        wifi: 1,
+        ngaydang: '01/10/2016',
+        giatien: 2000000
+      },
+      {
+        id: 3,
+        hinhanh: 'assets/img/index-03.jpg',
+        diachi: '123 abc P.15 Quận Gò Vấp',
+        songuoi: 2,
+        dientich: 25,
+        gioitinh: 'nam',
+        truong: 'PTIT',
+        wifi: 1,
+        ngaydang: '01/10/2016',
+        giatien: 2000000
+      },
+      {
+        id: 4,
+        hinhanh: 'assets/img/index-03.jpg',
+        diachi: '123 abc P.15 Quận Gò Vấp',
+        songuoi: 2,
+        dientich: 25,
+        gioitinh: 'nam',
+        truong: 'PTIT',
+        wifi: 1,
+        ngaydang: '01/10/2016',
+        giatien: 2000000
+      },
+      {
+        id: 5,
+        hinhanh: 'assets/img/index-03.jpg',
+        diachi: '123 abc P.15 Quận Gò Vấp',
+        songuoi: 2,
+        dientich: 25,
+        gioitinh: 'nam',
+        truong: 'PTIT',
+        wifi: 1,
+        ngaydang: '01/10/2016',
+        giatien: 2000000
+      }
+    ];
   }
 
 }
